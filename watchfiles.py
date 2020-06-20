8b981cedf2472d8725834304634d730865555cb8
qryhub_laptop <37242302+qryhub@users.noreply.github.com>
2020-06-20 17:15:40 +0200

    add scp over ssh functionality via paramiko

--- watchfiles.py
+++ watchfiles.py
@@ -1,6 +1,21 @@
 import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+import os
+import paramiko
+import json
+
+scp = None
+hostname = ""
+port = 0
+username = ""
+password = ""
+local = ""
+destination = ""
+
+## create ssh client
+ssh = SSHClient()
+ssh.load_system_host_keys()
 
 ## create event handler
 # properties
@@ -14,6 +29,8 @@ eventHandler = PatternMatchingEventHandler(patterns, ignorePatterns, ignorePatte
 # events
 def on_created(event):
     print(f"hey, {event.src_path} has been created!")
+    print(f"moving {event.src_path} to {destination}...")
+    scp.put(event.src_path, destination + event.src_path)
 
 def on_deleted(event):
     print(f"what the f**k! Someone deleted {event.src_path}!")
@@ -23,6 +40,23 @@ def on_modified(event):
 
 def on_moved(event):
     print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+
+def connect_scp():
+    print("connecting scp.. check your scp-config.json if this fails...")
+    # read properties from scp config
+    with open('scp-config.json', 'r' as config):
+        data = config.read()
+        obj = json.loads(data)
+        hostname = str(obj['hostname'])
+        port = int(obj['port'])
+        username = str(obj['username'])
+        password = str(obj['password'])
+        local = str(obj['local-path'])
+        destination = str(obj['destination-path'])
+    # connect ssh
+    ssh.connect(hostname=hostname, port=port, username=username, password=password)
+    # create scp connection
+    scp = SCPClient(ssh.get_transport())
 
 # assign events
 eventHandler.on_created = on_created
@@ -44,6 +78,7 @@ if __name__ == "__main__":
     print("hey, just ignore me... I'm just waiting for file changes...")
     # start the observer
     observer.start()
+    connect_scp()
     try:
         while True:
             time.sleep(1)
@@ -51,4 +86,5 @@ if __name__ == "__main__":
         # end observer
         observer.stop()
         observer.join()
+        scp.close()
     print("have a nice day bud :)")
